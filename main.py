3bf156c6194bd61c600fc0c2b4cb8e611f05512a
P.T Nyamuda <ptnrlab@gmail.com>
2024-11-05 05:55:05 +0200

    huggingface inference added

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
-from pydantic import BaseModel
 from fastapi import FastAPI
+from pydantic import BaseModel
+from huggingface_hub.inference_api import InferenceApi
 
 app = FastAPI()
 
-# Load the fine-tuned model and tokenizer only once
-tokenizer = AutoTokenizer.from_pretrained("google-t5/t5-small")
-model = AutoModelForSeq2SeqLM.from_pretrained("Tatenda/ClarityCrate")
-
-# Set up the pipeline with optional device argument (0 for GPU, -1 for CPU)
-generation_pipeline = pipeline("text2text-generation", model=model, tokenizer=tokenizer, device=-1)
+API_TOKEN = "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"
+# Use your custom model hosted on Hugging Face
+inference = InferenceApi(repo_id="Tatenda/ClarityCrate", token=API_TOKEN)
 
 class SummarizationRequest(BaseModel):
     text: str
 
 @app.post("/summarize")
 async def summarize_text(request: SummarizationRequest):
-    # Use the pipeline with streamlined settings
-    output = generation_pipeline(
-        f"summarize: {request.text}",
-        max_length=128,
-        num_beams=2,  # Try reducing to 2 beams or using greedy decoding
-        early_stopping=True
-    )
-    return {"output": output[0]["generated_text"]}
+    response = inference(f"summarize: {request.text}", params={"max_length": 128})
+    return {"output": response[0]["summary_text"]}
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "world"}
